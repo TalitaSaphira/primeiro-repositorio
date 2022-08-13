0481ce0b07d26e9fb1c11fd5641f070f555e8462
Talita Félix Lúcio <talitafelixlucio2004@gmail.com>
2022-08-13 12:09:51 -0300

    Nova função 'resultado'

--- testcode.cpp
+++ testcode.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 using namespace std;
 
+std::vector resultado (float media){
+    if (media < 40)
+        return "REPROVADO\n";
+    else if ((media >=40)&&(media <60))
+        return "EXAME ESPECIAL\n";
+    else if ((media >=60)&&(media<90))
+        return "APROVADA\n";
+    else 
+        return "APROVADA COM MÉRITOS\n";
+}
+
 int main (){
     int nota1, nota2, nota3;
     float media;
     cout << "Insira abaixo as notas correspondentes ao primeiro, segunto e terceiro trimeste:\n";
     cin >> nota1 >> nota2 >> nota3;
     media = (nota1*1 + nota2*2 + nota3*3) / 6;
-    if (media < 40)
-        cout << "REPROVADO\n";
-    else if ((media >=40)&&(media <60))
-        cout << "EXAME ESPECIAL\n";
-    else if ((media >=60)&&(media<90))
-        cout << "APROVADA\n";
-    else 
-        cout << "APROVADA COM MÉRITOS\n";
+    cout << "Resultado: " << resultado (media) << "\n";
     return 0;
 }
